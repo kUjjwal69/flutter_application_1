deadb3ca532a68f8ad681cf666022fd0115b31a9
kUjjwal69 <kumarujjwal4200@gmail.com>
2023-10-29 21:47:54 +0530

    DAY 2 COMPLETED

---
+++ lib/PAGES/homePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String value = "days of flutter";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days $value"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/PAGES/homePage.dart';
+import 'package:flutter_application_1/PAGES/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,14 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String value = "days of flutter";
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/Login",
+      routes: {
+        "/": (context) => Homepage(),
+        "/home": (context) => Homepage(),
+        "/Login": (context) => LoginPage()
+      },
     );
   }
 }
