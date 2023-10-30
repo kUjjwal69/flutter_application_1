9750263f1e80fddb6acc91ca8bdcd241f7221c75
kUjjwal69 <kumarujjwal4200@gmail.com>
2023-10-30 20:11:51 +0530

    DAY 7 COMPLETED

--- lib/PAGES/homePage.dart
+++ lib/PAGES/homePage.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/MODELS/catalog.dart';
+import 'package:flutter_application_1/WIDGETS/item_widgets.dart';
 
 import '../WIDGETS/drawers.dart';
 
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String value = "days of flutter";
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days $value"),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(
+          color: Colors.black,
         ),
+        title: Center(child: Text("Catalog App")),
+      ),
+      body: ListView.builder(
+        itemCount:dummyList.length, // Access the 'items' property in 'CatalogModel'.
+        itemBuilder: (context, index) {
+          return ItemWidget(
+            item: dummyList[index],
+            key: UniqueKey(),);
+        },
       ),
       drawer: MyDrawer(),
     );
