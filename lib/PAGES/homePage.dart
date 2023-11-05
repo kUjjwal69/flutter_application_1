7ef904847dd48dfc8d9a4a9c06f3ed44286d5276
kUjjwal69 <kumarujjwal4200@gmail.com>
2023-11-05 20:01:28 +0530

    DAY 10 COMPLETED

--- lib/PAGES/homePage.dart
+++ lib/PAGES/homePage.dart
@@ -44,7 +44,65 @@ class _HomepageState extends State<Homepage> {
       body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
+              ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
+
+                 itemBuilder: (context, index){
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                      ),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(
+                          (item.name),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        
+                        padding: const EdgeInsets.all(12),
+                        
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),),
+                      child: Image.network(
+                        item.image,
+                         ),
+                      footer: Container(
+                        child: Text(
+                          (item.price.toString()
+                          ),
+                          style: TextStyle(
+                            color: Colors.white
+                            ),
+                        ),
+                        
+                        padding: const EdgeInsets.all(12),
+                        
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        ),
+                    ),
+                  );
+                 },
+                 itemCount: CatalogModel.items.length
+                 )
+              : Center(
+                  child: CircularProgressIndicator(),
+                )
+                ),
+      drawer: MyDrawer(),
+    );
+  }
+}
+/*
+ListView.builder(
                   itemCount: CatalogModel.items
                       .length, // Access the 'items' property in 'CatalogModel'.
                   itemBuilder: (context, index) =>
@@ -53,12 +111,5 @@ class _HomepageState extends State<Homepage> {
                       key: UniqueKey(),
                     )
                     
-                )
-              : Center(
-                  child: CircularProgressIndicator(),
-                )),
-      drawer: MyDrawer(),
-    );
-  }
-}
+                )*/
 // 4:23:14
